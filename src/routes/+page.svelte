93b8ae96f0392ab40d3c623c489f076dde95abfd
TheRuky <renato@simple-code.hr>
2024-09-02 13:54:07 +0200

    feat: minimal updates to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { accio } from '$lib/accio';
+
+	$effect(() => {
+		accio('https://jsonplaceholder.typicode.com/posts/1')
+			.json()
+			.then(([data]) => {
+				console.log(data);
+			});
+	});
+</script>
+
+<h1>Accio</h1>
+<p>Coming soon...</p>
